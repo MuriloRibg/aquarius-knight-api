a20517e67a4a0a5706f45d9e7a33b5d78421fde7
Murilo Ribeiro <88835239+MuriloRibg@users.noreply.github.com>
2022-09-17 23:29:28 -0300

    Corrige método de inserir

--- Application/Models/Person.go
+++ Application/Models/Person.go
@@ -1,10 +1,16 @@
 package Models
 
-import "time"
+import (
+	"gorm.io/gorm"
+	"time"
+)
 
 type Person struct {
-	IdPerson  int       `form:"IdPerson" json:"IdPerson" xml:"IdPerson"  binding:"required"`
-	Fname     string    `form:"FirstName" json:"FirstName" xml:"FirstName"`
-	Lname     string    `form:"LastName" json:"LastName" xml:"LastName"`
-	BirthDate time.Time `form:"BirthDate" json:"BirthDate" xml:"BirthDate"`
+	IdPerson  int            `gorm:"primaryKey;autoIncrementIncrement" json:"IdPerson"`
+	Fname     string         `gorm:"column:Fname" json:"FirstName"`
+	Lname     string         `gorm:"column:Lname" json:"LastName"`
+	BirthDate *time.Time     `gorm:"column:BirthDate;type:date" json:"-"`
+	CreatedAt time.Time      `gorm:"column:CreatedAt;type:datetime" json:"-"`
+	UpdatedAt time.Time      `gorm:"column:UpdatedAt;type:datetime" json:"-"`
+	DeletedAt gorm.DeletedAt `gorm:"index;column:DeletedAt;type:datetime" json:"-"`
 }
